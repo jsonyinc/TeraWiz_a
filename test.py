c5d4f5adb82bf2c992b3cd371c4f5ee3cc7f01a0
Jason <jsonyinc@gmail.com>
2025-03-03 11:42:15 +0900

    Modify: 2nd Commit

--- test.py
+++ test.py
@@ -13,3 +13,4 @@ student2 = Student("영희", 22)
 # 메서드 호출
 print(student1.introduce())  # 출력: 안녕하세요, 저는 철수이고, 나이는 20입니다.
 print(student2.introduce())  # 출력: 안녕하세요, 저는 영희이고, 나이는 22입니다.
+print(student1.introduce())
